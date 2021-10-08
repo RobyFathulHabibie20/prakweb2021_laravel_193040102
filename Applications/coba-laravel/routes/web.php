591e639286223279df9d5a09efa88cd392004d90
RobyFathulHabibie20 <63381738+RobyFathulHabibie20@users.noreply.github.com>
2021-10-08 11:11:07 +0700

    Menyelesaikan vidio ke 4 membuat Blade Templating Enginee

--- Applications/coba-laravel/routes/web.php
+++ Applications/coba-laravel/routes/web.php
@@ -14,17 +14,69 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    return view('home', [
+        "title" => "Home"
+    ]);
 });
 
 Route::get('/about', function () {
     return view('about', [
+        "title" => "About",
         "name" => "Roby Fathul Habibie",
         "email" => "robyturbidity456@gmail.com",
         "image" => "me.jpeg"
     ]);
 });
 
+
 Route::get('/blog', function () {
-    return view('posts');
+    $blog_posts = [
+        [
+            "title" => "Judul Post Pertama",
+            "slug" => "judul-post-pertama",
+            "author" => "Roby F Habibie",
+            "body" => "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Optio praesentium quidem fuga vel eveniet repellat tempore, ut magni. Iusto, vel provident atque illo doloribus autem illum porro recusandae quas reiciendis laborum veniam a incidunt, in similique quia quo. Accusamus iure voluptas quasi dignissimos autem qui eos. Doloremque voluptatum aut et fugit consequuntur, repellat totam recusandae suscipit possimus fugiat officia veritatis beatae nobis eveniet temporibus praesentium tempore minima, dicta quis maxime."
+        ],
+        [
+            "title" => "Judul Post Kedua",
+            "slug" => "judul-post-kedua",
+            "author" => "Thomas Shelby",
+            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa natus laudantium beatae esse ipsam corporis numquam necessitatibus, corrupti distinctio! Voluptates nisi itaque harum quam. Quod mollitia accusamus maiores nemo placeat commodi iure itaque cumque nam non sapiente quae nihil error, dolores aut totam dolorem corporis repellendus sed rem optio! Esse eligendi quidem perferendis voluptas qui, sunt veritatis aliquid accusantium architecto inventore, ratione quam dolores at nulla fugit iure vitae ipsum iste autem eius totam nam? Quae voluptas rerum reiciendis, quo deserunt ipsam quam consequatur dolor ipsum, minima, nisi quaerat rem."
+        ],
+    ];
+
+    return view('posts', [
+        "title" => "Posts",
+        "posts" => $blog_posts
+    ]);
+});
+
+// Halamna Single Posts
+Route::get('posts/{slug}', function($slug) {
+    $blog_posts = [
+        [
+            "title" => "Judul Post Pertama",
+            "slug" => "judul-post-pertama",
+            "author" => "Roby F Habibie",
+            "body" => "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Optio praesentium quidem fuga vel eveniet repellat tempore, ut magni. Iusto, vel provident atque illo doloribus autem illum porro recusandae quas reiciendis laborum veniam a incidunt, in similique quia quo. Accusamus iure voluptas quasi dignissimos autem qui eos. Doloremque voluptatum aut et fugit consequuntur, repellat totam recusandae suscipit possimus fugiat officia veritatis beatae nobis eveniet temporibus praesentium tempore minima, dicta quis maxime."
+        ],
+        [
+            "title" => "Judul Post Kedua",
+            "slug" => "judul-post-kedua",
+            "author" => "Thomas Shelby",
+            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa natus laudantium beatae esse ipsam corporis numquam necessitatibus, corrupti distinctio! Voluptates nisi itaque harum quam. Quod mollitia accusamus maiores nemo placeat commodi iure itaque cumque nam non sapiente quae nihil error, dolores aut totam dolorem corporis repellendus sed rem optio! Esse eligendi quidem perferendis voluptas qui, sunt veritatis aliquid accusantium architecto inventore, ratione quam dolores at nulla fugit iure vitae ipsum iste autem eius totam nam? Quae voluptas rerum reiciendis, quo deserunt ipsam quam consequatur dolor ipsum, minima, nisi quaerat rem."
+        ],
+    ];
+
+    $new_post = [];
+    foreach($blog_posts as $post) {
+        if($post["slug"] === $slug) {
+            $new_post = $post;
+        }
+    }
+
+    return view('post', [
+        "title" => "Single Post",
+        "post" => $new_post
+    ]);
 });
