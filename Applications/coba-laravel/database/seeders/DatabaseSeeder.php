da165bd1f7dbdf994347844d307fcd1b6ce751a6
RobyFathulHabibie20 <63381738+RobyFathulHabibie20@users.noreply.github.com>
2021-10-22 13:40:56 +0700

    Menyelesaikan vidio ke 9 mengenai Database Seeder

--- Applications/coba-laravel/database/seeders/DatabaseSeeder.php
+++ Applications/coba-laravel/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Category;
+use App\Models\Post;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +17,63 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+
+        User::create([
+            'name' => 'Roby Fathul Habibie',
+            'email' => 'robyhabibie@gmail.com',
+            'password' => bcrypt('12345')
+        ]);
+
+        User::create([
+            'name' => 'Sandhika Galih',
+            'email' => 'sandhikagalih@gmail.com',
+            'password' => bcrypt('12345')
+        ]);
+
+        Category::create([
+            'name' => 'Web Programming',
+            'slug' => 'web-programming'
+        ]);
+
+        Category::create([
+            'name' => 'Personal',
+            'slug' => 'personal'
+        ]);
+
+        Post::create([
+            'title' => 'Judul Pertama',
+            'slug' => 'judul-pertama',
+            'excerpt' => 'Lorem ipsum Delectus nemo ad, dicta fuga lkajio omnis soluta modi vitae at laboriosam. Quae accusamus velita odit hic quaerat maiores.',
+            'body' => '<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Delectus nemo ad, dicta fuga lkajio omnis soluta modi vitae at laboriosam. Alias at quis beatae iusto eos. Quae accusamus velita odit hic quaerat maiores. Fugit nihil officia reiciendis eligendi tempora! Aspernaturdiandae,quod officiis deserunt doloribus sed,</p><p> fugiat culpa at deleniti, ab in facilis voluptate?omnis voluptatibus alias molestiae! Voluptates voluptatibus laudantium beatae accusamus aperiam. ew Sit nisi voluptates nesciunt porro magni iste eligendi, placeat voluptatem repellat, ut alias laudantium id. Suscipit saepe harum ducimus necessitatibus ex porro maiores quia doloribus ghaccusantium,</p><p>eius alias dolorum officiis dicta corporis vero. Numquam porro quod amet iure exodit hic quaerat maiores. Fugit nihil officia reiciendis eligendi tempora! Aspernaturdiandae, tenetur possimus exercitationem dicta. Alias nesciunt eius odio quo quisquam ipsa tempore id?</p>',
+            'category_id' => 1,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'title' => 'Judul Ke Dua',
+            'slug' => 'judul-ke-dua',
+            'excerpt' => 'Lorem ipsum Delectus nemo ad, dicta fuga lkajio omnis soluta modi vitae at laboriosam. Quae accusamus velita odit hic quaerat maiores.',
+            'body' => '<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Delectus nemo ad, dicta fuga lkajio omnis soluta modi vitae at laboriosam. Alias at quis beatae iusto eos. Quae accusamus velita odit hic quaerat maiores. Fugit nihil officia reiciendis eligendi tempora! Aspernaturdiandae,quod officiis deserunt doloribus sed,</p><p> fugiat culpa at deleniti, ab in facilis voluptate?omnis voluptatibus alias molestiae! Voluptates voluptatibus laudantium beatae accusamus aperiam. ew Sit nisi voluptates nesciunt porro magni iste eligendi, placeat voluptatem repellat, ut alias laudantium id. Suscipit saepe harum ducimus necessitatibus ex porro maiores quia doloribus ghaccusantium,</p><p>eius alias dolorum officiis dicta corporis vero. Numquam porro quod amet iure exodit hic quaerat maiores. Fugit nihil officia reiciendis eligendi tempora! Aspernaturdiandae, tenetur possimus exercitationem dicta. Alias nesciunt eius odio quo quisquam ipsa tempore id?</p>',
+            'category_id' => 1,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'title' => 'Judul Ke Tiga',
+            'slug' => 'judul-ke-tiga',
+            'excerpt' => 'Lorem ipsum Delectus nemo ad, dicta fuga lkajio omnis soluta modi vitae at laboriosam. Quae accusamus velita odit hic quaerat maiores.',
+            'body' => '<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Delectus nemo ad, dicta fuga lkajio omnis soluta modi vitae at laboriosam. Alias at quis beatae iusto eos. Quae accusamus velita odit hic quaerat maiores. Fugit nihil officia reiciendis eligendi tempora! Aspernaturdiandae,quod officiis deserunt doloribus sed,</p><p> fugiat culpa at deleniti, ab in facilis voluptate?omnis voluptatibus alias molestiae! Voluptates voluptatibus laudantium beatae accusamus aperiam. ew Sit nisi voluptates nesciunt porro magni iste eligendi, placeat voluptatem repellat, ut alias laudantium id. Suscipit saepe harum ducimus necessitatibus ex porro maiores quia doloribus ghaccusantium,</p><p>eius alias dolorum officiis dicta corporis vero. Numquam porro quod amet iure exodit hic quaerat maiores. Fugit nihil officia reiciendis eligendi tempora! Aspernaturdiandae, tenetur possimus exercitationem dicta. Alias nesciunt eius odio quo quisquam ipsa tempore id?</p>',
+            'category_id' => 2,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'title' => 'Judul Ke Empat',
+            'slug' => 'judul-ke-empat',
+            'excerpt' => 'Lorem ipsum Delectus nemo ad, dicta fuga lkajio omnis soluta modi vitae at laboriosam. Quae accusamus velita odit hic quaerat maiores.',
+            'body' => '<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Delectus nemo ad, dicta fuga lkajio omnis soluta modi vitae at laboriosam. Alias at quis beatae iusto eos. Quae accusamus velita odit hic quaerat maiores. Fugit nihil officia reiciendis eligendi tempora! Aspernaturdiandae,quod officiis deserunt doloribus sed,</p><p> fugiat culpa at deleniti, ab in facilis voluptate?omnis voluptatibus alias molestiae! Voluptates voluptatibus laudantium beatae accusamus aperiam. ew Sit nisi voluptates nesciunt porro magni iste eligendi, placeat voluptatem repellat, ut alias laudantium id. Suscipit saepe harum ducimus necessitatibus ex porro maiores quia doloribus ghaccusantium,</p><p>eius alias dolorum officiis dicta corporis vero. Numquam porro quod amet iure exodit hic quaerat maiores. Fugit nihil officia reiciendis eligendi tempora! Aspernaturdiandae, tenetur possimus exercitationem dicta. Alias nesciunt eius odio quo quisquam ipsa tempore id?</p>',
+            'category_id' => 2,
+            'user_id' => 2
+        ]);
     }
 }
