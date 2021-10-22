cf163011397425e4b3f234bd4b0d26db19ad02dd
RobyFathulHabibie20 <63381738+RobyFathulHabibie20@users.noreply.github.com>
2021-10-22 15:16:56 +0700

    Menyelesaikan vidio ke 10 mengenai Factory & Faker

--- Applications/coba-laravel/database/seeders/DatabaseSeeder.php
+++ Applications/coba-laravel/database/seeders/DatabaseSeeder.php
@@ -16,9 +16,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-
-        User::create([
+        
+     /*   User::create([
             'name' => 'Roby Fathul Habibie',
             'email' => 'robyhabibie@gmail.com',
             'password' => bcrypt('12345')
@@ -28,7 +27,9 @@ class DatabaseSeeder extends Seeder
             'name' => 'Sandhika Galih',
             'email' => 'sandhikagalih@gmail.com',
             'password' => bcrypt('12345')
-        ]);
+        ]); */
+
+        User::factory(3)->create();
 
         Category::create([
             'name' => 'Web Programming',
@@ -38,9 +39,11 @@ class DatabaseSeeder extends Seeder
         Category::create([
             'name' => 'Personal',
             'slug' => 'personal'
-        ]);
+        ]); 
 
-        Post::create([
+        Post::factory(20)->create(); 
+
+    /*    Post::create([
             'title' => 'Judul Pertama',
             'slug' => 'judul-pertama',
             'excerpt' => 'Lorem ipsum Delectus nemo ad, dicta fuga lkajio omnis soluta modi vitae at laboriosam. Quae accusamus velita odit hic quaerat maiores.',
@@ -74,6 +77,6 @@ class DatabaseSeeder extends Seeder
             'body' => '<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Delectus nemo ad, dicta fuga lkajio omnis soluta modi vitae at laboriosam. Alias at quis beatae iusto eos. Quae accusamus velita odit hic quaerat maiores. Fugit nihil officia reiciendis eligendi tempora! Aspernaturdiandae,quod officiis deserunt doloribus sed,</p><p> fugiat culpa at deleniti, ab in facilis voluptate?omnis voluptatibus alias molestiae! Voluptates voluptatibus laudantium beatae accusamus aperiam. ew Sit nisi voluptates nesciunt porro magni iste eligendi, placeat voluptatem repellat, ut alias laudantium id. Suscipit saepe harum ducimus necessitatibus ex porro maiores quia doloribus ghaccusantium,</p><p>eius alias dolorum officiis dicta corporis vero. Numquam porro quod amet iure exodit hic quaerat maiores. Fugit nihil officia reiciendis eligendi tempora! Aspernaturdiandae, tenetur possimus exercitationem dicta. Alias nesciunt eius odio quo quisquam ipsa tempore id?</p>',
             'category_id' => 2,
             'user_id' => 2
-        ]);
+        ]); */
     }
 }
